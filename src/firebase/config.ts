79391b2079ea8271fc75dd791a51c85e466023c3
Rishi Chalana <rishichalana2@gmail.com>
2025-10-24 11:37:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-229314306-7fa51",
+  "appId": "1:561948977727:web:074a86dd1d04fc845b268a",
+  "apiKey": "AIzaSyCG9A9nyDMLYW57ne1QU-HEj_nWekcmCmo",
+  "authDomain": "studio-229314306-7fa51.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "561948977727"
+};
